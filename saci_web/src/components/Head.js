1f056b95c5fcedbc4af3a454294165f85eb3324b
Diego Lopez Ariza <diegoawesome2017@outlook.com>
2023-03-09 13:58:17 -0600

    nuevo header

---
+++ saci_web/src/components/Head.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const Header = () => {
+  return (
+    <Head>
+        <title>SACI</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <Meta name="description" content="La web de saci se encarga de mostrar estadisticas acerca del estado de tus cultivos de agricultura protegida" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default Header
